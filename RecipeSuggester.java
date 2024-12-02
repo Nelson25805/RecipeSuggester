0e612921659ee86012908df47e53c714326d2222
Nelson25805 <91099339+Nelson25805@users.noreply.github.com>
2024-12-01 21:46:59 -0500

    Added initial starter code for project in recipesuggester

---
+++ RecipeSuggester.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RecipeSuggester{
+    
+}
