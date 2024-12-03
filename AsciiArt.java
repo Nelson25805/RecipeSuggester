0db675cb4672872eb6a9a27eade7dd11d755c242
Nelson25805 <91099339+Nelson25805@users.noreply.github.com>
2024-12-02 22:23:12 -0500

    Added Ascii art java file for printing ascii artwork

---
+++ AsciiArt.java
@@ -0,0 +1,29 @@
+public class AsciiArt {
+
+    // Method to display fridge ASCII art
+    public static void showFridgeArt() {
+        String fridge = """
+         _______________________
+        |                       |
+        |  What ingredients do  |
+        |       you have?       |
+        |                    () |
+        |_______________________|
+        |_______________________|
+        |                       |
+        |   .-------.           |
+        |   |       |           |
+        |   |  | |  |           |
+        |   |__|_|__|        () |
+        |                       |
+        |                       |
+        |                       |
+        |                       |
+        |                       |
+        |_______________________| 
+        """;
+        System.out.println(fridge);
+    }
+
+    // Add more artwork methods here if needed
+}
